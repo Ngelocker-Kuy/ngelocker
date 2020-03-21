6c4f6bcbce16096c66c9ad471a0ecf5970e9a1db
hafizulrifkihawari <hafizulrifkihawari@gmail.com>
2020-03-21 16:08:57 +0700

    add functionality

--- client/web/src/containers/PermissionPage.jsx
+++ client/web/src/containers/PermissionPage.jsx
@@ -3,7 +3,11 @@ import "../styles/permission.css";
 import { ErrorPermission, SuccessPermission } from '.'
 
 function PermissionPage() {
+<<<<<<< HEAD
     const dummyData = true
+=======
+    const dummyData = false
+>>>>>>> add functionality
     return (
         <div id="container">
             {
