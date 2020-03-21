0eaae27f6138b9d13c3223abbf2b358d734c636d
azputra <ahmadzp102@gmail.com>
2020-03-21 17:46:41 +0700

    modified:   src/containers/RegisterGuestPage.jsx

--- client/web/src/containers/RegisterGuestPage.jsx
+++ client/web/src/containers/RegisterGuestPage.jsx
@@ -1,42 +1,32 @@
 import React from 'react';
-import "../styles/formRegister.css";
 
-function RegisterUserPage() {
+function RegisterGuestPage() {
     return (
         <div className="register">
             <div className="container">
-                <div className="row justify-content-center mt-5">
-                    <div className="col-5 text-center">
-                        <h1>Daftarkan User!</h1>
-                        <p>Isi dengan benar dan lengkapi</p>
-                        <lottie-player
-                            src="https://assets7.lottiefiles.com/packages/lf20_rRm3Ew.json" background="transparent" speed="1" style={{ width: "120vw", height: "57vh", cursor: "pointer" }} autoplay loop >
-                        </lottie-player>
-                    </div>
-                    <div className="col-5">
+                <lottie-player 
+                src="https://assets7.lottiefiles.com/packages/lf20_rRm3Ew.json" 
+                background="transparent" 
+                speed="1" 
+                style={{ width: "120vw", height: "57vh", cursor: "pointer" }} 
+                autoplay loop > 
+                </lottie-player>
+                <div className="row justify-content-center mt-5" style={{ marginTop: "8rem!important", flexDirection: "column" }}>
+                    <div className="col">
                         <div className="user-accounts-form shadow p-5">
                             <div className="user-accounts-form-header">
-                                <h2 className="title">Daftarkan Sekarang</h2>
-                                <p className="text">
-                                    Sudah Punya Locker?
-                                    <a className="link-page"> check locker</a>
-                                </p>
+                                <h2 className="title">Isi Data Diri</h2>
                             </div>
                             <form>
                                 <div className="form-group">
-                                    <label>Username</label>
+                                    <label>Name</label>
                                     <input type="text" className="form-control" />
                                 </div>
                                 <div className="form-group">
-                                    <label>Email</label>
+                                    <label>Phone Number</label>
                                     <input type="text" className="form-control" />
-                                    <p className="user-input-info">Contoh: email@ngelocker.com</p>
                                 </div>
-                                <div className="form-group">
-                                    <label>Password</label>
-                                    <input type="password" className="form-control" />
-                                </div>
-                                <button type="submit" className="btn-register btn-block mt-4">Daftar</button>
+                                <button type="submit" className="btn-register btn-block mt-4">Submit</button>
                             </form>
                         </div>
                     </div>
@@ -45,4 +35,4 @@ function RegisterUserPage() {
         </div >
     )
 }
-export default RegisterUserPage
+export default RegisterGuestPage
