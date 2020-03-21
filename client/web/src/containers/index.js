762e73c3d1d0556d53305552dc5157f9b8f45808
azputra <ahmadzp102@gmail.com>
2020-03-21 16:26:31 +0700

    resolve conflict di app.js dan containers

--- client/web/src/containers/index.js
+++ client/web/src/containers/index.js
@@ -1,6 +1,7 @@
 export { default as LoginAdminPage } from './LoginAdminPage'
 export { default as ListUsersPage } from './ListUsersPage'
 export { default as PermissionPage } from './PermissionPage'
+export { default as RegisterUserPage } from './RegisterUserPage'
 
 export { default as SideBar } from '../components/Sidebar'
 export { default as Card } from '../components/Card'
