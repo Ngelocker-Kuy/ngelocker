9445a1faeb9b894ee31af9610e1b1add5fc7182c
fajrinnr <fajrin.noorrachman11@gmail.com>
2020-03-21 19:27:18 +0700

    Done nih

--- client/web/src/containers/WaitingScreen.jsx
+++ client/web/src/containers/WaitingScreen.jsx
@@ -1,21 +1,30 @@
 import React from "react";
-import Lottie from "react-lottie";
-import * as animationData from "./5081-empty-box.json";
 
 function WaitingScreen() {
-  console.log(animationData);
   return (
-    <div style={{ backgroundColor: "black" }}>
-      <h1>WAITING FOR PERMISSION</h1>
+    <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', padding: '50px'}}>
+      <h1 style={{textAlign: 'center', color: 'black'}}>WAITING FOR PERMISSION</h1>
+      <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'center'}}>
       <lottie-player
-        src="https://assets10.lottiefiles.com/packages/lf20_5gSzGx.json"
+        src="https://assets7.lottiefiles.com/packages/lf20_4gycw9.json"
         background="transparent"
         speed="1"
-        style="width: 300px; height: 300px;"
+        style={{width: '100%', height: '100%', alignSelf: 'center', position: 'absolute', marginTop: '20vh'}}
         loop
-        controls
         autoplay
       ></lottie-player>
+      
+      </div>
+      <div>
+      <lottie-player
+        src="https://assets3.lottiefiles.com/datafiles/cb81834f3b75c3d2aba9d8a58ad1f408/AE_JSON/loader1.json"
+        background="transparent"
+        speed="1"
+        style={{width: '100%', height: '100%', alignSelf: 'center', marginTop: '30vh'}}
+        loop
+        autoplay
+      ></lottie-player>
+      </div>
     </div>
   );
 }
