e79f8cc9fbc10cb2d3c3d0db2b6432c96677e397
hafizulrifkihawari <hafizulrifkihawari@gmail.com>
2020-03-23 20:56:29 +0700

    socket implementation

--- client/mobile/components/itemCard.jsx
+++ client/mobile/components/itemCard.jsx
@@ -5,50 +5,50 @@ import axios from '../services/axios'
 // import Btn from "./Btn";
 
 function generateStyle(color) {
-  return StyleSheet.create({
-    item: {
-      backgroundColor: color,
-      padding: 20,
-      marginVertical: 8,
-      marginHorizontal: 16
-    },
-    name: {
-      fontSize: 28,
-      color: "snow",
-      textTransform: "capitalize"
-    },
-    phone: {
-      fontSize: 24,
-      color: "#f9d423"
-    },
-    btnAccept: {
-      borderRadius: 5,
-      backgroundColor: "#52c234",
-      alignItems: "center",
-      marginVertical: 2
-    },
-    btnDecline: {
-      borderRadius: 5,
-      backgroundColor: "#f83600",
-      alignItems: "center",
-      marginVertical: 2
-    }
-  });
+    return StyleSheet.create({
+        item: {
+            backgroundColor: color,
+            padding: 20,
+            marginVertical: 8,
+            marginHorizontal: 16
+        },
+        name: {
+            fontSize: 28,
+            color: "snow",
+            textTransform: "capitalize"
+        },
+        phone: {
+            fontSize: 24,
+            color: "#f9d423"
+        },
+        btnAccept: {
+            borderRadius: 5,
+            backgroundColor: "#52c234",
+            alignItems: "center",
+            marginVertical: 2
+        },
+        btnDecline: {
+            borderRadius: 5,
+            backgroundColor: "#f83600",
+            alignItems: "center",
+            marginVertical: 2
+        }
+    });
 }
 
 function Item({ title, phoneNumber, status, type }) {
-  let color;
+    let color;
 
-  console.log(status);
-  if (status === null) {
-    color = "#83a4d4";
-  } else if (status) {
-    color = "green";
-  } else {
-    color = "#f12711";
-  }
+    console.log(status);
+    if (status === null) {
+        color = "#83a4d4";
+    } else if (status) {
+        color = "green";
+    } else {
+        color = "#f12711";
+    }
 
-  const styles = generateStyle(color);
+    const styles = generateStyle(color);
 
     const unlockLocker = async (status) => {
         const token = await AsyncStorage.getItem('token')
@@ -71,20 +71,25 @@ function Item({ title, phoneNumber, status, type }) {
 
     return (
         <View style={styles.item}>
-      <Text style={styles.name}>{title}</Text>
-      <Text style={styles.phone}>{phoneNumber}</Text>
-      {/* button */}
-      <TouchableOpacity style={styles.btnAccept}>
-        <Text>
-          <h3>Accept</h3>
-        </Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.btnDecline}>
-        <Text>
-          <h3>Decline</h3>
-        </Text>
-      </TouchableOpacity>
-    </View>
+            <Text style={styles.name}>{title}</Text>
+            <Text style={styles.phone}>{phoneNumber}</Text>
+            {/* button */}
+            {type === "request" ?
+                <>
+                    <TouchableOpacity style={styles.btnAccept}>
+                        <Text>
+                            <h3>Accept</h3>
+                        </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.btnDecline}>
+                        <Text>
+                            <h3>Decline</h3>
+                        </Text>
+                    </TouchableOpacity>
+                </>
+                : null
+            }
+        </View>
     );
 }
 
