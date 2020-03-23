48ad78dada50c6dcdd37c586ace603d2ba5a5fa4
hafizulrifkihawari <hafizulrifkihawari@gmail.com>
2020-03-23 21:52:57 +0700

    socket done

--- client/mobile/components/Btn.jsx
+++ client/mobile/components/Btn.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, TouchableOpacity, Text } from "react-native";
+import { StyleSheet, TouchableOpacity, Text, View } from "react-native";
 
 function Btn(props) {
   return (
