31f4ec206a9f788a5f236a8b0c6717d00f56345c
fajrinnr <fajrin.noorrachman11@gmail.com>
2020-03-25 15:44:34 +0700

    Fixing Permissions

--- client/web/src/containers/PermissionPage.jsx
+++ client/web/src/containers/PermissionPage.jsx
@@ -5,14 +5,13 @@ import { useHistory } from 'react-router-dom'
 import { ErrorPermission, SuccessPermission } from '.'
 
 function PermissionPage() {
-    // const statusLock = useSelector(state => state.lockerReducers.status  )
+    const statusLock = useSelector(state => state.lockerReducers.status  )
     const guest = useSelector(state => state.guestReducers.guest)
-    const statusLock = false
     const history = useHistory()
 
-    // if (Object.keys(guest).length === 0) {
-    //     history.goBack()
-    // }
+    if (Object.keys(guest).length === 0) {
+        history.goBack()
+    }
 
     return (
         <div id="container" style={{ marginTop: '7vh', display: 'flex', flexDirection: 'row', justifyContent: 'center', height: '100%', overflow: 'hidden' }}>
