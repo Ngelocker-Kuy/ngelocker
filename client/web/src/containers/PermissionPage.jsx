fac6e7e94ff7810f7db2a10779e4099ed5897b26
fajrinnr <fajrin.noorrachman11@gmail.com>
2020-03-25 15:40:51 +0700

    Styling React Web Done

--- client/web/src/containers/PermissionPage.jsx
+++ client/web/src/containers/PermissionPage.jsx
@@ -5,17 +5,17 @@ import { useHistory } from 'react-router-dom'
 import { ErrorPermission, SuccessPermission } from '.'
 
 function PermissionPage() {
-    const statusLock = useSelector(state => state.lockerReducers.status)
+    // const statusLock = useSelector(state => state.lockerReducers.status  )
     const guest = useSelector(state => state.guestReducers.guest)
-
+    const statusLock = false
     const history = useHistory()
 
-    if (Object.keys(guest).length === 0) {
-        history.goBack()
-    }
+    // if (Object.keys(guest).length === 0) {
+    //     history.goBack()
+    // }
 
     return (
-        <div id="container" style={{ marginTop: '7vh' }}>
+        <div id="container" style={{ marginTop: '7vh', display: 'flex', flexDirection: 'row', justifyContent: 'center', height: '100%', overflow: 'hidden' }}>
             {
                 statusLock ?
                     <SuccessPermission />
